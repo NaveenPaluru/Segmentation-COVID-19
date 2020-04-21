a0d00363cd621958c0ab7c076f648140c3e7a4ec
Naveen Paluru <34019192+NaveenPaluru@users.noreply.github.com>
2020-04-21 19:34:00 +0530

    Add files via upload

---
+++ data preparation/dataPrepCTVolume.m
@@ -0,0 +1,139 @@
+% Data Preparation for Test Set 2
+
+% Data Taken from: http://medicalsegmentation.com/covid19/
+% Segmentation dataset nr. 2 (13th April) Image volumes (308 Mb) – 9 volumes, total of > 800 slices
+%                                         Covid19 masks (0.3 Mb) – includes  > 350 annotated slices
+%                                         Lung masks of (1.0 Mb) – includes  > 700 annotated slices
+
+% lung masks by Johannes Hofmanninger:  https://github.com/JoHof/lungmask
+% 
+% Phaneendra Yalavarthy
+% Created on: March 31, 2020; Updated by Naveen Paluru on: April 18, 2020
+% 
+
+
+% Do Some clean up
+
+clc; clear all; close all;
+
+% Download the files from above repositoroes and unzip them and add all the
+% paths to matlab.
+
+addpath(genpath(['./']));
+
+% Get all the files with .nii extension
+
+mskni = dir('rp_msk/*.nii'); lngmk= dir('rp_lung_msk/*.nii');
+imnii  = dir('rp_im/*.nii' );
+
+% Make a bounding box to ccrop the images and labels from 630x630 to
+% 512x512.
+
+r = centerCropWindow2d([630,630],[512,512]);
+
+% Two empty mtrices for preparing test data
+
+IMG = [];
+LAB = [];
+
+% iterate over all the 9 volumes
+
+for file = 1:length(mskni(:))
+    
+    image= niftiread(strcat(imnii(file).folder,'/', imnii(file).name));  
+    mask = niftiread(strcat(mskni(file).folder,'/', mskni(file).name));
+    lmsk = niftiread(strcat(lngmk(file).folder,'/', lngmk(file).name));
+    
+    % make a directory for current volume
+    
+    mkdir(num2str(file));
+    
+    % Go to the created directory (This is just to save the images here for
+    % visualization)
+    
+    cd(num2str(file))
+    
+    % iterate over all slices in the current voume
+    
+    for j = 1:size(image,3)   
+        
+        % Take one frame
+        frame = squeeze(image(:,:, j));  
+        
+        % Lung mask (make sure it is a binary mask)
+        mlim1 = squeeze(lmsk(:,:, j));
+        mlim1(mlim1(:)>1) = 1;        
+        
+        % Make background 0figure;
+        frame = frame.*double(mlim1);
+        
+        % It is tilted, so transpose
+        image_scan_original = frame';
+        image_scan_original = uint8(255 * mat2gray(image_scan_original));
+        
+         % Background
+        ml = squeeze(mask(:,:, j));
+        ml0 = zeros(size(ml));
+        ml0(ml(:) == 0) = 1;
+        
+
+        % Label for Ground Glass Opacity (GGO)
+        ml1 = zeros(size(ml));
+        ml1(ml(:) == 1) = 1;
+        ml1 = imbinarize(ml1');
+
+        % Label for Pleural Effusion
+        ml3 = zeros(size(ml));
+        ml3(ml(:) == 3) = 1;
+        ml3 = imbinarize(ml3');
+
+        % Label for Consolidation
+        ml2 = zeros(size(ml));
+        ml2(ml(:) == 2) = 1;
+        ml2 = imbinarize(ml2');
+        
+        
+        % combine all labels 1, 2, 3 data into 1 (Abnormal labels)
+        ml12 = imbinarize(ml2 + ml1 + ml3);        
+        pos = ml12;
+        
+        % Make normal labels
+        neg = logical(mlim1').*imcomplement(ml12);
+        neg(neg==1)=2;        
+        
+        % Combile both labels (0:bg, 1:Abnrml, 2:nrml)
+        lab = pos+neg;
+        
+        
+        % Now crop the image and label to 512 x512
+        imag  = imcrop(image_scan_original,r);
+        labl  = imcrop(lab,r);
+        
+        % A small check that ensures we are not involving only background
+        % in the test set
+        
+         if length(unique(lab))>1 && sum(lab>0,'all')>100
+            IMG = cat(3,IMG, imag);
+            LAB = cat(3,LAB, labl);
+         end
+        
+        % This will save all the slices of each volume into  separate folders.
+        % Change the flag Visible from off to on to view images while execting.
+        
+        fig=figure('rend','painters','pos', [50 , 300, 1500, 600],'Visible', 'off');
+        subplot(121);imshow(mat2gray(imag));
+        xlabel('Image');
+        subplot(122);imshow(mat2gray(labl));
+        xlabel('GT');
+        saveas(fig, strcat(num2str(j),'.jpg'));
+        close all;       
+    end
+    
+    % Come out of current volume directory
+    cd ../
+end
+
+% Finally Save IMG and LAB in testVOL.mat file. This mat file should have the:
+% IMG : 512x512x704
+% LAB : 512x512x704
+% Use testVOL.mat file as Test Set 2.% For Test Set 1 check dataPrepCTSlices.m
---
+++ data preparation/dataprepCTSlices.m
@@ -0,0 +1,131 @@
+% Data Preparation for Train and Test Set 1
+
+% Data Taken from: http://medicalsegmentation.com/covid19/
+% Data Download link :Training images as .nii.gz (151.8 Mb)  – 100 slices
+%                     Training  masks as .nii.gz (1.4 Mb)    – 100 masks
+% lung masks by Johannes Hofmanninger: Lung masks as .nii.gz - 100 masks
+% (0.3Mb). 
+% https://github.com/JoHof/lungmask
+% 
+
+
+% Phaneendra Yalavarthy
+% Created on: March 31, 2020; Updated by Naveen Paluru on: April 18, 2020
+% 
+
+
+% Do some clean up
+clear all;
+close all
+clc; 
+% 
+%
+%
+%% Load the data
+
+% CT slice data
+im = niftiread('tr_im.nii');
+
+% Labeled Data
+mim = niftiread('tr_mask.nii'); %and
+
+% Lung Mask data
+mlim = niftiread('tr_lungmasks.nii');
+
+% Number of Slices
+noslices = size(im, 3);
+%% Now Run it for available no of slices available
+for i = 1:100
+    
+    % Take one frame
+    frame = squeeze(im(:,:, i));
+    
+    % Lung mask (make sure it is a binary mask)
+    mlim1 = squeeze(mlim(:,:, i));
+    mlim1(mlim1(:)>1) = 1;
+    
+    % Make background 0
+    frame = frame.*double(mlim1);
+    % It is tilted, so transpose
+    image_scan_original = frame';
+    [m, n] = size(frame');
+    
+    % Convert to unit8 (DO NOT USE unit8 command, it does not work!)
+    image_scan = uint8(255 * mat2gray(image_scan_original));
+    
+        
+    % Background
+    ml = squeeze(mim(:,:, i));
+    ml0 = zeros(size(ml));
+    ml0(ml(:) == 0) = 1;
+       
+    % Label for Ground Glass Opacity (GGO)
+    ml1 = zeros(size(ml));
+    ml1(ml(:) == 1) = 1;
+    ml1 = imbinarize(ml1');
+       
+    % Label for Pleural Effusion
+    ml3 = zeros(size(ml));
+    ml3(ml(:) == 3) = 1;
+    ml3 = imbinarize(ml3');
+        
+    % Label for Consolidation
+    ml2 = zeros(size(ml));
+    ml2(ml(:) == 2) = 1;
+    ml2 = imbinarize(ml2');
+    
+    
+    % combine all labels 1, 2, 3 data into 1 (Abnormal Labels)
+    ml12 = imbinarize(ml2 + ml1 + ml3);
+    positive = ml12;
+     
+     % Normal Labels
+     negative = logical(mlim1').*imcomplement(ml12);
+     negative(negative==1)=2;
+     
+     
+     % Overall Labels
+     labels = positive+negative;
+     
+     
+     % Augmentation
+     
+     orgi = image_scan;        % Original
+     fhi  = flip(orgi,1);      % Flip Horizontal
+     fvi  = flip(orgi,2);      % Flip Vertical
+    
+    
+     
+    % Similarly Do for labels
+     
+     orgl = labels;
+     fhl  = flip(orgl,1);
+     fvl  = flip(orgl,2);
+    
+     
+     % Concatenate the data
+     
+
+      if i == 1
+          inp = cat(3,orgi,fhi,fvi);
+          lab = cat(3,orgl,fhl,fvl);
+      else
+          inp = cat(3,inp,orgi,fhi,fvi);
+          lab = cat(3,lab,orgl,fhl,fvl);
+          
+      end
+  
+      
+      % Finally split inp, lab into train and test. Train data 270 slices
+      % and Test Data set 1 with  30 slices.
+      
+      
+      % train.mat should have inp : 512x512x270, lab:512x512x270
+      % test.mat  should have inp : 512x512x 30, lab:512x512x 30
+      % test.mat is the Test Set 1.
+      % For Test Set 2 check dataPrepCTVolumes.m
+
+           
+end
+
+
